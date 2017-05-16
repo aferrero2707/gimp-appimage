bb2d73b06fb7c8f67a99d3f47bc3d912c7f7eb5e
aferrero2707 <aferrero1975@gmail.com>
2017-05-16 10:40:48 +0200

    INitial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+*** Dependencies ***
+
+zlib (zlib1g-dev in Ubuntu)
+python (libpython-all-dev in Ubuntu)
+libffi (libffi-dev in Ubuntu)
+libpcre3-dev
+lzma-dev
+libjbig-dev
+libicu-dev
+bison + libbison-dev
+libgmp-dev
+libbzip2-dev
---
+++ build.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+mkdir -p work
+cd work
+
+WD=$(pwd)
+prefix=$WD/inst
+
+if [ ! -e "$prefix/bin" ]; then
+    (rm -rf jhbuild && git clone git://git.gnome.org/jhbuild && cd jhbuild && ./autogen.sh --prefix=$prefix && make && make install)
+fi
+
+export PATH=$prefix/bin:$PATH
+export LD_LIBRARY_PATH=$prefix/lib:$LD_LIBRARY_PATH
+export CHECKOUTROOT=$WD/sources
+
+
+jhbuild -f "$WD/../gimp.jhbuildrc" -m "$WD/../modulesets/gimp.modules" build --distclean gimp-bootstrap
+jhbuild -f "$WD/../gimp.jhbuildrc" -m "$WD/../modulesets/gimp.modules" build --distclean --skip harfbuzz,liblzma,json-c $*
