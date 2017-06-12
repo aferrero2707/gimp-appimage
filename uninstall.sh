6eb100f9f55560c87846786697492e7e273ff81f
aferrero2707 <aferrero1975@gmail.com>
2017-06-12 20:27:23 +0200

    Added nUFraw; added script for AppImage creation without network

---
+++ uninstall.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+mkdir -p work
+cd work
+
+WD=$(pwd)
+prefix=$WD/inst
+
+export PATH=/zzz/bin:$prefix/bin:$PATH
+export LD_LIBRARY_PATH=/zzz/lib:$prefix/lib:$LD_LIBRARY_PATH
+
+export CHECKOUTROOT=$WD/sources
+
+jhbuild -f "$WD/../gimp.jhbuildrc" -m "$WD/../modulesets/gimp.modules" uninstall $*
