0e13f8c9538417b315eb559ee71871d6c187c141
aferrero2707 <aferrero1975@gmail.com>
2017-06-18 18:46:10 +0200

    Fixed building issues

--- build.sh
+++ build.sh
@@ -12,4 +12,4 @@ export LD_LIBRARY_PATH=/zzz/lib:$prefix/lib:$LD_LIBRARY_PATH
 export CHECKOUTROOT=$WD/sources
 export BUILDROOT=$WD/build
 
-jhbuild -f "$WD/../gimp.jhbuildrc" -m "$WD/../modulesets/gimp.modules" build --skip harfbuzz,liblzma,json-c $*
+jhbuild -f "$WD/../gimp.jhbuildrc" -m "$WD/../modulesets/gimp.modules" build --skip harfbuzz,liblzma $*
