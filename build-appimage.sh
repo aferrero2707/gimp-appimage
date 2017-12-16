becc33ab37795e9ce3f24a7e3e63c0fe410d1f13
aferrero2707 <aferrero1975@gmail.com>
2017-12-16 18:48:40 +0100

    Updated appimage build script

--- build-appimage.sh
+++ build-appimage.sh
@@ -3,13 +3,15 @@
 export PKG_CONFIG_PATH=/app/share/pkgconfig:$PKG_CONFIG_PATH
 export ACLOCAL_PATH=/app/share/aclocal:$ACLOCAL_PATH
 
-jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" buildone --exit-on-error json-glib && \
-jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" buildone --exit-on-error poppler-data && \
-jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" buildone ---exit-on-error babl && \
-jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" buildone --exit-on-error gegl && \
-jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" buildone --exit-on-error libmypaint && \
-jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" buildone --exit-on-error gimp && \
-jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" buildone --exit-on-error phfgimp && \
-jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" buildone --exit-on-error nufraw && \
-jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" buildone --exit-on-error resynthesizer && \
-jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" buildone --exit-on-error gimp-lqr-plugin
+cp /sources/modulesets/gnome-external-deps-2.32.modules /work/conf/modulesets
+
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" --exit-on-error buildone json-glib && \
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" --exit-on-error buildone poppler-data && \
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" --exit-on-error buildone babl && \
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" --exit-on-error buildone gegl && \
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" --exit-on-error buildone libmypaint && \
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" --exit-on-error buildone gimp && \
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" --exit-on-error buildone phfgimp && \
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" --exit-on-error buildone nufraw && \
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" --exit-on-error buildone resynthesizer && \
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" --exit-on-error buildone gimp-lqr-plugin
