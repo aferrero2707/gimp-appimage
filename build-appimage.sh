f96c2d9694390399d4c295d1f9886aa019d1a909
aferrero2707 <aferrero1975@gmail.com>
2017-12-16 11:27:33 +0100

    Update build-appimage.sh

--- build-appimage.sh
+++ build-appimage.sh
@@ -2,13 +2,13 @@
 
 PKG_CONFIG_PATH=/app/share/pkgconfig:$PKG_CONFIG_PATH
 
-jhbuild -f "/work/conf/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build json-glib poppler-data
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build json-glib poppler-data
 
-jhbuild -f "/work/conf/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build -t babl babl
-jhbuild -f "/work/conf/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build -t gegl gegl
-jhbuild -f "/work/conf/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build -t libmypaint libmypaint
-jhbuild -f "/work/conf/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build -t gimp gimp
-jhbuild -f "/work/conf/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build -t phfgimp phfgimp
-jhbuild -f "/work/conf/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build -t nufraw nufraw
-jhbuild -f "/work/conf/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build -t resynthesizer resynthesizer
-jhbuild -f "/work/conf/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build -t gimp-lqr-plugin gimp-lqr-plugin
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build -t babl babl
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build -t gegl gegl
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build -t libmypaint libmypaint
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build -t gimp gimp
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build -t phfgimp phfgimp
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build -t nufraw nufraw
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build -t resynthesizer resynthesizer
+jhbuild -f "/sources/gimp.jhbuildrc" -m "/work/conf/modulesets/gimp.modules" build -t gimp-lqr-plugin gimp-lqr-plugin
