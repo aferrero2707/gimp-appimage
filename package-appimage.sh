37fe6e50d4556de4ab47fc38e8ab5e5da059775d
aferrero2707 <aferrero1975@gmail.com>
2017-12-19 22:17:20 +0100

    Added time in version string

--- package-appimage.sh
+++ package-appimage.sh
@@ -31,7 +31,7 @@ pwd
 
 #VER1=$(pkg-config --modversion gimp-2.0)-test
 #VER1=$(pkg-config --modversion gimp-2.0)-$(date +%Y%m%d)
-VER1=$(pkg-config --modversion gimp-2.0)${VER_SUFFIX}-$(date +%Y%m%d)
+VER1=$(pkg-config --modversion gimp-2.0)${VER_SUFFIX}-$(date +%Y%m%d)_$(date +%H%M)
 if [ x"$FULL_BUNDLING" = "x1" ]; then
     VER1="${VER1}-full"
 fi
