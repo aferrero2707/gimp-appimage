f46c18f48dae3f8fea65b72ec03f47dd95fe9e5a
aferrero2707 <aferrero1975@gmail.com>
2018-12-12 22:15:18 +0100

    Update mkappimage.sh

--- mkappimage.sh
+++ mkappimage.sh
@@ -11,7 +11,7 @@ cp -a /sources/appimage-helper-scripts/bundle-gtk2.sh "${APPROOT}/scripts" &&
 cp -a /sources/appimage-helper-scripts/bundle-python.sh "${APPROOT}/scripts") || exit 1
 
 # fill and package the AppImage bundle
-bash /sources/package-appimage.sh || exit 1
+#bash /sources/package-appimage.sh || exit 1
 #exit
 
 (mkdir -p "${APPROOT}/scripts" && cp -a /sources/scripts/gmic.sh "${APPROOT}/scripts") || exit 1
