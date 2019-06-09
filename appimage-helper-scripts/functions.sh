dafb5e3959d6042bdf1574bb3b78e64c0ab6dcb3
aferrero2707 <aferrero1975@gmail.com>
2019-06-09 20:20:10 +0200

    Added debugging messages

--- appimage-helper-scripts/functions.sh
+++ appimage-helper-scripts/functions.sh
@@ -119,6 +119,7 @@ copy_deps2()
   PWD=$(readlink -f .)
   FILES=$(find . -type f -executable -or -name *.so.* -or -name *.so | sort | uniq )
   for FILE in $FILES ; do
+    echo "copy_deps2: collecting dependencies of \"${FILE}\""
     ldd "${FILE}" | grep "=>" | awk '{print $3}' | xargs -I '{}' echo '{}' >> DEPSFILE
   done
   DEPS=$(cat DEPSFILE | sort | uniq)
