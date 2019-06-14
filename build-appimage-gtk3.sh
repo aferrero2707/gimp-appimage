53656857c84100e9b77cb5c07c96fec5b1ba68de
aferrero2707 <aferrero1975@gmail.com>
2019-06-14 13:27:58 +0200

    Update build-appimage-gtk3.sh

--- build-appimage-gtk3.sh
+++ build-appimage-gtk3.sh
@@ -7,45 +7,12 @@ export LD_LIBRARY_PATH=/${AIPREFIX}/lib64:/${AIPREFIX}/lib:$LD_LIBRARY_PATH
 #(yum update -y && yum install -y epel-release && yum update -y && yum install -y libtool-ltdl-devel autoconf automake libtools which json-c-devel json-glib-devel gtk-doc gperf libuuid-devel libcroco-devel) || exit 1
 (yum-config-manager --add-repo http://www.nasm.us/nasm.repo && yum update -y && yum install -y suitesparse-devel libunwind-devel libwmf-devel openjpeg2-devel libmng-devel libXpm-devel iso-codes-devel mercurial numactl-devel nasm gnome-common libappstream-glib-devel poppler-glib-devel) || exit 1
 
-if [ ! -e /work/x265-done ]; then
-(cd /work && rm -rf x265 && hg clone http://hg.videolan.org/x265 && \
-cd x265 && mkdir -p build && cd build && \
-cmake -DCMAKE_INSTALL_PREFIX=/${AIPREFIX} ../source && make -j 2 install) || exit 1
-
-(cd /work && rm -rf libde265 && \
-git clone -b frame-parallel https://github.com/strukturag/libde265.git && \
-cd libde265 && mkdir build && ./autogen.sh && ./configure --prefix=/${AIPREFIX} --disable-dec265 --disable-sherlock265 && make -j 2 install)
-touch /work/x265-done
-fi
-
-if [ ! -e /work/heif-done ]; then
-(cd /work && rm -rf libheif && git clone -b v1.3.2 https://github.com/strukturag/libheif.git && \
-cd libheif && ./autogen.sh && ./configure --prefix=/${AIPREFIX}  && make -j 2 install) || exit 1
-touch /work/heif-done
-fi
-
-if [ ! -e /work/webp-done ]; then
-# WebP - Windows Image Components not supported
-echo "967b8f087cb392e6cc94d5e116a120c0  libwebp-1.0.0.tar.gz" > /work/libwebp-1.0.0.hash
-(cd /work && rm -rf libwebp-1.0.0 && \
-wget http://downloads.webmproject.org/releases/webp/libwebp-1.0.0.tar.gz &&\
-md5sum --check libwebp-1.0.0.hash && tar xvf libwebp-1.0.0.tar.gz && \
-cd libwebp-1.0.0 && ./configure --prefix=/${AIPREFIX} --enable-everything && make -j 2 install) || exit 1
-touch /work/webp-done
-fi
 
 
-if [ ! -e /work/poppler-done ]; then
-#(cd /work && rm -rf freetype-2.9 && \
-#wget https://download.savannah.gnu.org/releases/freetype/freetype-2.9.tar.gz && \
-#tar xf freetype-2.9.tar.gz && cd freetype-2.9 && \
-#./configure --prefix=/${AIPREFIX} && make -j 2 install) || exit 1
-ldd /zyx/lib/libcairo.so
-(cd /work && rm -rf poppler-0.74.0 && wget https://poppler.freedesktop.org/poppler-0.74.0.tar.xz && \
-tar xf poppler-0.74.0.tar.xz && cd poppler-0.74.0 && mkdir -p build && cd build && \
-Freetype_DIR=/${AIPREFIX} Fontconfig_DIR=/${AIPREFIX} cmake -DCMAKE_PREFIX_PATH=/${AIPREFIX} -DCMAKE_INSTALL_PREFIX=/${AIPREFIX} .. && \
-make -j 3 install) || exit 1
-touch /work/poppler-done
+if [ ! -e /work/libarchive-done ]; then
+(cd /work && rm -rf libarchive* && wget https://github.com/libarchive/libarchive/releases/download/v3.4.0/libarchive-3.4.0.tar.gz && \
+tar xf libarchive-3.4.0.tar.gz && cd libarchive* && ./configure --prefix=/${AIPREFIX} && make -j 2 install) || exit 1
+touch /work/libarchive-done
 fi
 
 
